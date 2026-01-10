dce47c9182162ff23511cf0dd3a6cc0e0d28d1e5
Vivek Jha <vivekjha0905@gamil.com>
2026-01-11 03:19:30 +0530

    Fix Firebase analytics unused variable

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.tsx
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCdml16d3VhbfuQ4uulQ33UlDdh8vL2fn0",
   authDomain: "portfolio-79e38.firebaseapp.com",
@@ -14,10 +10,16 @@ const firebaseConfig = {
   storageBucket: "portfolio-79e38.firebasestorage.app",
   messagingSenderId: "999371222627",
   appId: "1:999371222627:web:4e0f807c0ed950237a5256",
-  measurementId: "G-71D62EKDPC"
+  measurementId: "G-71D62EKDPC",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// ✅ EXPORT analytics (fixes ESLint error)
+export const analytics = getAnalytics(app);
+
+// Firestore
 export const db = getFirestore(app);
+
+export default app;
